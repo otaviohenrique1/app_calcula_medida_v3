d766ec44191aaffd34a9b1d1fc52ca52e03a396c
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-12-09 10:05:41 -0300

    commit 1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{
+        flexDirection: 'row',
+        alignItems: "center",
+        gap: 10,
+      }}>
+        <TextInput keyboardType='number-pad' placeholder='Campo A' />
+        <Text>----</Text>
+        <TextInput keyboardType='number-pad' placeholder='Campo B' />
+      </View>
+      <View style={{
+        flexDirection: 'row',
+        alignItems: "center",
+        gap: 10,
+      }}>
+        <TextInput keyboardType='number-pad' placeholder='Campo C' />
+        <Text>----</Text>
+        <TextInput keyboardType='default' placeholder='Campo X' />
+      </View>
+      <Text>Resultado</Text>
       <StatusBar style="auto" />
     </View>
   );
